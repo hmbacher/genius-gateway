f89384075550711eb7f7b4b2c1044f220a4e0440
Hans <hmbacher@gmail.com>
2025-09-14 11:16:39 +0200

    Optimized device card visualization

--- interface/src/lib/components/DeviceStatusCard.svelte
+++ interface/src/lib/components/DeviceStatusCard.svelte
@@ -1,46 +1,65 @@
 <script lang="ts">
 	import type { GeniusDevice } from '$lib/types/models';
+	import { GeniusDeviceRegistration } from '$lib/types/enums';
 	import IconDetector from '~icons/custom-icons/smoke-detector-2xl';
 	import IconNumber from '~icons/tabler/number';
 	import IconFactory from '~icons/tabler/building-factory-2';
 	import IconBell from '~icons/tabler/bell';
 	import IconHeart from '~icons/tabler/heart';
 	import IconFire from '~icons/tabler/flame-filled';
+	import IconExternal from '~icons/tabler/external-link';
 
 	interface Props {
 		detector: GeniusDevice;
 	}
 
 	let { detector }: Props = $props();
-	let isUnknownLocation = detector.location === 'Unknown location';
+	let isForeign = detector.registration === GeniusDeviceRegistration.GeniusPacket;
 </script>
 
 <div
-	class="rounded-box shadow-primary/50 relative max-w-120 overflow-hidden shadow-lg p-5 {detector.isAlarming
-		? 'bg-error'
-		: isUnknownLocation
-			? 'bg-warning'
-			: 'bg-base-200'}"
+	class="rounded-box shadow-primary/50 relative w-full max-w-120 overflow-hidden shadow-lg p-5 {detector.isAlarming
+		? 'bg-error text-error-content'
+		: isForeign
+			? 'bg-secondary text-secondary-content'
+			: 'bg-primary text-primary-content'}"
 >
 	<div class="flex">
 		<div class="shrink-0">
-			<IconDetector class="h-20 w-24 text-base-content/60" />
+			<IconDetector class="h-20 w-24 text-current/60" />
 		</div>
 	</div>
 
 	<div class="absolute top-4 right-4">
 		{#if !detector.isAlarming}
-			<IconHeart class="h-6 w-6 text-success" />
+			<IconHeart class="h-6 w-6" />
 		{:else}
 			<IconFire class="h-6 w-6" />
 		{/if}
 	</div>
 
-	<div class="text-xl font-medium {isUnknownLocation ? 'text-warning-content/30 italic' : ''}">
+	{#if isForeign}
+		<div class="absolute top-10 right-4">
+			<div
+				class="tooltip tooltip-left"
+				data-tip="Foreign detector"
+			>
+				<IconExternal class="h-6 w-6 text-current/50" />
+			</div>
+		</div>
+	{/if}
+
+	<div class="text-xl font-medium">
 		{detector.location}
 	</div>
-	<div class="divider my-1"></div>
-	<div class="flex text-base-content/50 gap-5">
+	<div
+		class="divider my-1 {detector.isAlarming
+			? 'before:text-error-content/30 after:text-error-content/30'
+			: isForeign
+				? 'before:bg-secondary-content/30 after:bg-secondary-content/30'
+				: 'before:bg-primary-content/30 after:bg-primary-content/30'}"
+	></div>
+	<div class="flex text-current/70 gap-5">
 		<span class="inline-flex">
 			<IconNumber class="mr-1 h-5 w-5" />
 			<span class="text-sm">{detector.smokeDetector.sn}</span>
@@ -57,7 +76,7 @@
 			{/if}
 		</span>
 	</div>
-	<div class="flex text-base-content/50 gap-5">
+	<div class="flex text-current/70 gap-5">
 		<div class="inline-flex">
 			<IconBell class="mr-1 h-5 w-5" />
 			<span class="text-sm">
