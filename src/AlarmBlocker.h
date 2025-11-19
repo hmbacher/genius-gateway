e625e99fd8ce3d558d1fcdf54a6918870d7807f2
Hans <hmbacher@gmail.com>
2025-11-19 12:07:49 +0100

    Updated Source code license information

--- src/AlarmBlocker.h
+++ src/AlarmBlocker.h
@@ -1,6 +1,30 @@
 /**
  * @file AlarmBlocker.h
  * @brief AlarmBlocker service for temporarily blocking alarm notifications
+ * 
+ * @copyright Copyright (c) 2024-2025 Genius Gateway Project
+ * @license AGPL-3.0 with Commons Clause
+ * 
+ * This file is part of Genius Gateway.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the Commons Clause restriction.
+ * 
+ * "Commons Clause" License Condition v1.0
+ * The Software is provided to you by the Licensor under the License,
+ * as defined below, subject to the following condition:
+ * Without limiting other conditions in the License, the grant of rights
+ * under the License will not include, and the License does not grant to you,
+ * the right to Sell the Software.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * See https://github.com/hmbacher/genius-gateway/blob/main/LICENSE for details.
  */
 
 #pragma once
