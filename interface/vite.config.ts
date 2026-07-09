90a1cce82a76abb01c39ac3cca6f0daeded8bff8
Hans <hmbacher@gmail.com>
2026-07-09 19:54:18 +0200

    chore(build): bump picomatch lockfile, update dev proxy target

--- interface/vite.config.ts
+++ interface/vite.config.ts
@@ -29,12 +29,12 @@ const config: UserConfig = {
 		proxy: {
 			// Proxying REST: http://localhost:5173/rest/bar -> http://192.168.1.83/rest/bar
 			'/rest': {
-				target: 'http://192.168.178.133',
+				target: 'http://192.168.178.122',
 				changeOrigin: true
 			},
 			// Proxying websockets ws://localhost:5173/ws -> ws://192.168.1.83/ws
 			'/ws': {
-				target: 'http://192.168.178.133',
+				target: 'http://192.168.178.122',
 				changeOrigin: true,
 				ws: true
 			}
